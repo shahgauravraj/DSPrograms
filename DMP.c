772adf41366ec23a84dd51ae31c24175c26f5a50
Gaurav <shahgauravraj@gmail.com>
2020-01-03 13:59:09 +0530

    Deepty shetty mam's problem solution

---
+++ DMP.c
@@ -0,0 +1,29 @@
+//This program counts the numbers of times each digit occur in a multi-digit number.
+
+#include <stdio.h>
+
+int main()
+{
+    int num, i;
+    int count[10] = {0, 0, 0, 0, 0, 0, 0 ,0 ,0, 0};
+    printf("Enter the number: ");
+    scanf("%d", &num);
+    
+    while(num != 0)
+    {
+        printf("%d\n", (num % 10));
+        for(i=0;i<10;i++)
+        {
+            if((num%10) == i)
+            {
+                count[i]++;
+            }
+        }
+        num = num / 10;
+    }
+    printf("Counts are given as: \n");
+    for(i=0;i<10;i++)
+        printf("%d\t", count[i]);
+
+    return 0;
+}
